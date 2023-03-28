c29a571cfe4cbbd8a8e3c6df5817c2a59320a73e
Marco Di Blasi <marco.diblasi.lutech@skytv.it>
2023-03-28 16:12:56 +0200

    Aggiungere i file di progetto.

---
+++ NewPackageManager_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NewPackageManager_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("NewPackageManager_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("EE1C4C30-41FB-454A-ADDC-15239C4F09F8")]
+[assembly: AssemblyVersion("1.0.0.0")]
